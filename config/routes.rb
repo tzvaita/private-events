4773ce69435beca4a62e907dde465389ffb369e2
tzvaita <tzvaita@gmail.com>
2020-04-23 12:11:09 +0200

    Add events controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/new'
+  get 'events/show'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
