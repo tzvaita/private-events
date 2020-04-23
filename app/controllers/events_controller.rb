663f0a45a22da50a061c24b8e0a2c56b78cd713f
tzvaita <tzvaita@gmail.com>
2020-04-23 14:53:30 +0200

    Add events methods and views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,28 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
 
   def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render 'new'
+    end
   end
 
   def show
+    @event = Event.find(params[:id])
   end
+
+  private
+
+      def event_params
+        params.require(:event).permit(:desc, :user_id)
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'events/index'
-  get 'events/new'
-  get 'events/show'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/signup', to: 'users#new'
   root 'application#hello'
   resources :users
+  resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
