d55716d5510a1764ccd43e463deea80cbafb3a83
tzvaita <tzvaita@gmail.com>
2020-04-24 19:38:25 +0200

    Add Attendance model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
-class Event < ApplicationRecord
+ class Event < ApplicationRecord
+   has_many :attendances, foreign_key: :attended_event_id
+   has_many :attendees, through: :attendances
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
 end
