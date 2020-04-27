249835a2feec8a7a0c3207d0bfad59245de708eb
tzvaita <tzvaita@gmail.com>
2020-04-27 19:02:21 +0200

    Add test for user model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,8 @@
    has_many :attendees, through: :attendances
    belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
 
+   validates :desc, presence: true
+
    scope :past, ->(current_time = DateTime.now) { where('date < ?', current_time) }
    scope :upcoming, ->(current_time = DateTime.now) { where('date >= ?', current_time) }
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before :each do
+    @user = User.create(name: 'myname')
+  end
+
+  context 'with valid details' do
+    it 'should create a user' do
+      expect(@user).to be_valid
+    end
+
+    it 'should have a name' do
+      expect(@user.name).to be_present
+    end
+  end
+
+  context 'with invalid details' do
+    it 'should not be valid' do
+      @user.name = '   '
+      expect(@user).to_not be_valid
+    end
+
+    it 'should have a unique name' do
+      duplicate_name = @user.dup
+      @user.save
+      expect(duplicate_name).to_not be_valid
+    end
+  end
+end
